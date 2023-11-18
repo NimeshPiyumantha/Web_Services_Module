0676b0619d9379d530d9ab2fbf39b101feefe9ff
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-11-18 16:33:36 +0530

    update Project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -13,3 +14,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+print(__name__)
+if __name__ == "__main__":
+    uvicorn.run(app)
