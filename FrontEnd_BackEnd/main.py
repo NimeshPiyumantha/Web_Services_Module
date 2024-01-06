5436d6112a875358c41a78ff39b40ce75c273555
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2024-01-06 10:55:25 +0530

    Add forth step

--- FrontEnd_BackEnd/main.py
+++ FrontEnd_BackEnd/main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
@@ -10,6 +10,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
